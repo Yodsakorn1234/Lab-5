66edb8063a1c9c4cf4935a77075994300b07f2e5
Yodsakorn1234 <menming1234@outlook.com>
2020-12-15 17:13:15 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
+    int x,y,i;
+     while(x!=0){
+          cout << "Enter an integer: ";
+          cin>>x;
+          if(x==0){break}
+          if(x%2==0){
+              y++
+
+          }
+else{
+    i++;
+}
+y++;
+     }
+
     cout << "#Even numbers = ";
     cout << "#Odd numbers = ";
     return 0;
